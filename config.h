23a4cdc2e601a75e0a60adec0106b5b2a4e6d239
Lobakkang <felixthian5053@gmail.com>
2023-05-18 18:52:29 +0800

    Added status2D thread function

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
 static const int horizpadbar        = 10;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 10;        /* vertical padding for statusbar */
-static const char *fonts[] = {"monospace:size=10"};
+static const char *fonts[] = {"BigBlueTerm437 Nerd Font:size=10"};
 static const char dmenufont[] = "monospace:size=10";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
